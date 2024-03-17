a851a89fd6192afad1860f98329b9f9a0c871fd3
Rohan Singh <rohsingh@pdx.edu>
2024-03-16 18:20:43 -0700

    Main python app file added

---
+++ src/main.py
@@ -0,0 +1,15 @@
+import sys
+from form import MainWidget
+from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import QSize
+
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
+if __name__ == "__main__":
+    app = QApplication([])
+    widget = MainWidget()
+    widget.setWindowTitle("Summarizer")
+    widget.setFixedSize(QSize(WINDOW_WIDTH, WINDOW_HEIGHT))
+    widget.show()
+    sys.exit(app.exec())
